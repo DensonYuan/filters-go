5cd85d392394ca542e10258b08bc325a43addb1d
yuandunsheng <yuandunsheng@kuaishou.com>
2020-05-30 15:44:59 +0800

    add func GetCount

--- func.go
+++ func.go
@@ -30,6 +30,11 @@ func (f *ModelFilter) GetQuerySet(db *gorm.DB) *gorm.DB {
 	return db
 }
 
+func (f *ModelFilter) GetCount(db *gorm.DB) (cnt int, err error) {
+	err = f.GetQuerySet(db).Limit(-1).Offset(0).Count(&cnt).Error
+	return
+}
+
 func (f *ModelFilter) Count(db *gorm.DB, value interface{}) *gorm.DB {
 	return f.GetQuerySet(db).Limit(-1).Offset(0).Count(value)
 }
